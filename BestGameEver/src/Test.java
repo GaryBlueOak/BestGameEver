98c2580d0d2231a846fa1858bc83687f58b46f94
GaryBlueOak <psmithdbs@gmail.com>
2014-12-28 16:20:50 -0500

    test3

--- BestGameEver/src/Test.java
+++
@@ -1,9 +0,0 @@
-import java.lang.*;
-
-public class Test {
-	public static void main(String[] args){
-		
-	System.out.print("hello");
-
-	}
-}
--- BestGameEver/src/Test3.java
+++ BestGameEver/src/Test3.java
@@ -1,5 +1,6 @@
 
 public class Test3 {
 	private int variable = 5;
-
+	private int var2 = 6;
+	private int var3 = 7;
 }
